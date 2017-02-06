0307d426fa5bb39fce18c8964643aa79ce85e474
Kais <kais.zgal@gmail.com>
2017-02-06 16:32:56 +0100

    added verification of login to navigation bar elements

--- app/components/navigation/navigation.js
+++ app/components/navigation/navigation.js
@@ -1,12 +1,36 @@
 'use strict';
 
 function NavigationBarCtrl() {
-	
+
 }
 
 angular.module('genestimate').component('genestimateNavigationBar', {
   templateUrl: 'components/navigation/navigation-bar.html',
-  controller: NavigationBarCtrl,
+  controller: ['$scope', '$location', 'AuthService', function($scope, $location, AuthService){
+
+      $scope.isAuthenticated = false;
+      $scope.isAdmin = false;
+      $scope.username = '';
+
+      $scope.$on('authChanged', function () {
+          $scope.isAuthenticated = AuthService.isAuthenticated();
+          $scope.isAdmin = AuthService.hasPermission("ADMIN");
+          if ($scope.isAuthenticated){
+              $scope.username = AuthService.getAuthData().authId;
+          }
+          else{
+              $scope.username = '';
+          }
+      });
+
+      $scope.logout = function () {
+
+          AuthService.logout().then(function (response) {
+              AuthService.setAuthData(undefined);
+              $location.path("/login");
+          });
+      }
+    }],
   bindings: {
   	links : '='
   }
