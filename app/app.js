d2fbfb369b8ab0a57f3d7939f0655d7332d1a6d7
Kais <kais.zgal@gmail.com>
2017-02-06 16:16:25 +0100

    adding new modules and HttpInterceptor

--- app/app.js
+++ app/app.js
@@ -2,13 +2,20 @@
 
 // Declare app level module which depends on views, and components
 angular.module('genestimate', [
-  'ngRoute',
-  'genestimate.login',
-  'genestimate.order',
-  'genestimate.version'
+    'ngRoute',
+    'genestimate.login',
+    'genestimate.order',
+    'genestimate.contact',
+    'genestimate.my-estimate-details',
+    'genestimate.estimates-list',
+    'genestimate.estimates-details',
+    'genestimate.my-estimates',
+    'genestimate.loading-directive',
+    'genestimate.version'
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+config(['$locationProvider', '$routeProvider', '$httpProvider', function($locationProvider, $routeProvider, $httpProvider) {
+    $httpProvider.interceptors.push('authHttpRequestInterceptor');
+    $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/login'});
 }]);
