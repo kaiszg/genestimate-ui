71d2d3c02eb4edd25750378f460bf2ff337ee41c
Kais <kais.zgal@gmail.com>
2017-02-06 16:27:25 +0100

    loading directive created

---
+++ app/components/loading/loading-directive.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Kais on 06.02.2017.
+ */
+'use strict';
+
+angular.module('genestimate.loading-directive', [])
+
+    .directive('loading',   ['$http' ,function ($http)
+    {
+        return {
+            restrict: 'A',
+            link: function (scope, elm, attrs)
+            {
+                scope.isLoading = function () {
+                    return $http.pendingRequests.length > 0;
+                };
+
+                scope.$watch(scope.isLoading, function (v)
+                {
+                    if(v){
+                        elm.show();
+                    }else{
+                        elm.hide();
+                    }
+                });
+            }
+        };
+
+    }]);
